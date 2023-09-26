0164f861cb7ff07e07c1849e9dedbd801cf5bd08
Shubham Mote <moteshubham@gmail.com>
2023-09-26 20:00:18 +1200

    removed errors

--- cpp_mote/19sept/2_date_class.cpp
+++ cpp_mote/19sept/2_date_class.cpp
@@ -8,8 +8,8 @@ class Date
 	int month;
 	int year;
 	public:
-	Date();
-	Date(int d, int m, int y) : day(d), month(m), year(y){};
+	Date(){}
+	Date(int d, int m, int y) : day(d), month(m), year(y){}
 	void Read()
 	{
 		    int dd,mm,yy;
@@ -19,10 +19,15 @@ class Date
 		    cin>>mm;
 		    cout<<"Enter Year:";
 		    cin>>yy;
+		    this->day=dd;
+		    this->month=mm;
+		    this->year=yy;
 	}
 	void Write()
 	{
-
+		    cout<<day<<" ";
+		    cout<<month<<" ";
+		    cout<<year<<endl;
 	}
 	bool operator==(Date obj2)
 	{
@@ -30,27 +35,26 @@ class Date
 			return true;
 		return false;
 	}
+	bool operator<=(Date obj2)
+	{
+		if( day <= obj2.day && month <= obj2.month && year <= obj2.year )
+			return true;
+		return false;
+	}
 };
 
 int main()
 {
-	//Date d3.read();
-	Date d1(22,7,1996);
-	Date d2(22,7,1997);
-			if (d1 == d2)
+	Date d3;
+	d3.Read();
+	Date d1 (22,7,1996);
+	Date d2(22,7,1995);
+		if (d1 <= d2)
 			cout<<" d1 = d2"<<endl;
 		else
 			cout<< "d1 != d2"<<endl;
 	
 	return 0;
 }
-
-
-
-
-
-
-
-
 
 
