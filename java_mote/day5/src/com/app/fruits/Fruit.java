d7208ec82d38d5068a48cb6324bee24f9563d86a
Shubham Mote <moteshubham@gmail.com>
2023-10-11 20:07:45 +0530

    final commit for fruitbasket

--- java_mote/day5/src/com/app/fruits/Fruit.java
+++ java_mote/day5/src/com/app/fruits/Fruit.java
@@ -27,7 +27,10 @@ public class Fruit {
 	public String getColor() {
 		return this.color;
 	}
-	public void setFreshFalse() {
+	public boolean getFresh() {
+		return this.fresh;
+	}
+	public void setFreshStale() {
 		 this.fresh = false;
 	}
 	public String toString() {
@@ -39,47 +42,6 @@ public class Fruit {
 
 /*
 
-4.1 Can you arrange Fruit,Apple,Orange,Mango in inheritance hierarchy ?
-Use tight encapsulation.
-
-4.2 Properties (instance variables)  : color : String , weight : double , name:String, fresh : boolean
-
-4.3 Add suitable constructor
-
-4.4 Override  toString correctly to return state of all fruits (return only  : name ,color , weight )
-
-4.5 Add a taste() method : public String taste()
-
-For Fruit : Can you identify taste of any general fruit ? NO
-So add a taste() with this definition : returns "no specific taste" 
-
-Apple : should return  "sweet n sour"
-Mango : should return  "sweet"
-Orange : should return  "sour"
-
-4.6 Add specific functionality , in the sub classes
-In Mango : public void pulp() {Display name n color of the fruit + a mesg  creating  pulp!}
-In Orange : public void juice() {Display name n weight of the fruit + a mesg extracting juice!}
-In Apple : public void jam() {Display name of the fruit + a mesg  making jam!}
-
-4.7 Add all of above classes under the package "com.app.fruits"
-4.8 Create java application FruitBasket , with main method , as a tester
-4.9 Prompt user for the basket size n create suitable data structure
-
-4.10 Supply options
-1. Add Mango
-2. Add Orange
-3. Add Apple
-NOTE : You will be ALWAYS adding a fresh fruit in the basket , in all of above options.
-
-4. Display names of all fruits in the basket.
-
-5. Display name,color,weight , taste of all fresh fruits , in the basket.
-
-
-6. Mark a fruit in a basket , as stale(=not fresh)
-i/p : index 
-o/p : error message (in case of invalid index) or mark it stale
 
 7. Mark all sour fruits stale 
 Hint : Use equals() method of the String class.
@@ -88,6 +50,5 @@ Hint : Use equals() method of the String class.
 i/p : index
 Invoke correct functionality (pulp / juice / jam)
 
-9. Exit 
 
  */
--- java_mote/day5/src/com/app/tester/FruitBasket.java
+++ java_mote/day5/src/com/app/tester/FruitBasket.java
@@ -14,52 +14,39 @@ public class FruitBasket {
 		
 		boolean exit = false;
 		while (!exit) {
-			System.out.println("1. Add Mango \n2. Add Orange \n3. Add Apple \n"
+			System.out.println("\n\n1. Add Mango \n2. Add Orange \n3. Add Apple \n"
 					+ "4. Display names of all fruits in the basket.\n"
 					+ "5. Display name,color,weight , taste of all fresh fruits , in the basket.\n"
 					+ "6. Mark a fruit in a basket , as stale(=not fresh)\n"
 					+ "7. Mark all sour fruits stale\n"
 					+ "8. Invoke fruit specific functionality (pulp / juice / jam)\n"
-					+ "0. Exit");
+					+ "0. Exit\n");
 			switch (sc.nextInt()) {
 			case 1:
 				if (counter >= 0 && counter < fruits.length) {
-//					System.out.println("Is the fruit fresh? true or false");
-//					if (sc.nextBoolean() == true) {
-//						System.out.println("Enter color weight name");
 						fruits[counter++] = new Mango();
-//					} else
-//						System.out.println("Bring fresh fruit please!!");
 				} else
 					System.out.println("Basket full");
 				break;
 			case 2:
 				if (counter >= 0 && counter < fruits.length) {
-//					System.out.println("Is the fruit fresh? true or false");
-//					if (sc.nextBoolean() == true) {
-//						System.out.println("Enter color wight name");
-						fruits[counter++] = new Orange();
-//					} else
-//						System.out.println("Bring fresh fruit please!!");
-				} else
+					fruits[counter++] = new Orange();
+				} 
+				else
 					System.out.println("Basket full");
 			
 				break;
 			case 3:
 				if (counter >= 0 && counter < fruits.length) {
-//					System.out.println("Is the fruit fresh? true or false");
-//					if (sc.nextBoolean() == true) {
-//						System.out.println("Enter color weight name");
 						fruits[counter++] = new Apple();
-//					} else
-//						System.out.println("Bring fresh fruit please!!");
-				} else
+				} 
+				else
 					System.out.println("Basket full");
 				break;
 			case 4:
 				for(Fruit f : fruits) {
 					if(f!=null)
-					System.out.println(f.getName());
+					System.out.println(f.getName()+" is "+ f.getFresh());
 				}
 				break;			
 								
@@ -68,25 +55,47 @@ public class FruitBasket {
 					if(f!=null)
 					System.out.println(f+f.taste());
 				}
-				
 				break;
 				
 			case 6:
 				System.out.println("Enter indice to make it stale :");
 				int index = sc.nextInt() -1 ;
-				fruts[index].set
-
+				if(index >=0 && index <counter) {
+					fruits[index].setFreshStale();
+					System.out.println("fruit: "+index+1+" marked stale");
+				}
+				else 
+					System.out.println("Invalid index");
 				break;
 				
 			case 7:
-			
+				for (int i = 0; i < fruits.length; i++) {
+					if(fruits[i] instanceof Orange)
+						fruits[i].setFreshStale();
+				}
 				break;
 				
 			case 8:
-				
+				System.out.println("Enter indice to make it stale :");
+				index = sc.nextInt() -1 ;
+				if(index >=0 && index <counter) {
+					if(fruits[index] instanceof Apple)
+						((Apple)(fruits[index])).jam();
+					else if(fruits[index] instanceof Orange)
+						((Orange)(fruits[index])).juice();
+					else
+						((Mango)(fruits[index])).pulp();
+			
+				}
+				else 
+					System.out.println("Invalid index");
+
 				break;
+			case 0:
+				exit = true;
 				
 			default:
+				System.out.println("bye");
 				break;
 			}
 		}
@@ -94,30 +103,7 @@ public class FruitBasket {
 	}
 
 }
-/* 4.3 Add suitable constructor
-* 
-* 4.4 Override toString correctly to return state of all fruits (return only :
-* name ,color , weight )
-* 
-
-* 
-* 4.6 Add specific functionality , in the sub classes In Mango : public void
-* pulp() {Display name n color of the fruit + a mesg creating pulp!} In Orange
-* : public void juice() {Display name n weight of the fruit + a mesg extracting
-* juice!} In Apple : public void jam() {Display name of the fruit + a mesg
-* making jam!}
-* 
-* 4.10 Supply options 1. Add Mango 2. Add Orange 3. Add Apple NOTE : You will
-* be ALWAYS adding a fresh fruit in the basket , in all of above options.
-* 
-* 4. Display names of all fruits in the basket.
-* 
-* 5. Display name,color,weight , taste of all fresh fruits , in the basket.
-* 
-* 
-* 6. Mark a fruit in a basket , as stale(=not fresh) i/p : index o/p : error
-* message (in case of invalid index) or mark it stale
-* 
+/*
 * 7. Mark all sour fruits stale Hint : Use equals() method of the String class.
 * 
 * 8. Invoke fruit specific functionality (pulp / juice / jam) i/p : index
