7856837e931ace67d9b0ae73cef5a61e75c1d05e
Shubham Mote <moteshubham@gmail.com>
2023-10-17 15:08:59 +0530

    java.time parse learned

---
+++ java_mote/day9/src/time_parsing/TimeParsing.java
@@ -0,0 +1,31 @@
+package time_parsing;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeParseException;
+import java.util.Scanner;
+
+public class TimeParsing {
+
+	
+
+	public static void main(String[] args) {
+
+		try (Scanner sc = new Scanner(System.in)) {
+			
+			LocalDate date = LocalDate.now();
+			LocalDate date2 = LocalDate.of(2023, 10, 15);
+			LocalDate date3 = LocalDate.parse(sc.next());
+			System.out.println(date);
+			System.out.println(date2);
+			System.out.println(date3);
+		} catch (DateTimeParseException e) {
+			System.out.println("getmsg");
+			e.getMessage();
+			System.out.println(e.hashCode());
+			;
+			System.out.println("stack");
+			e.printStackTrace();
+		}
+	}
+
+}
