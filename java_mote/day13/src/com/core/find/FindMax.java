3f9d6c7f7b6a0d5dae2afb709e4e4e883528579b
Shubham Mote <moteshubham@gmail.com>
2023-10-25 22:38:20 +0530

    generics learnt by example

--- java_mote/day13/src/com/core/find/FindMax.java
+++ java_mote/day13/src/com/core/find/FindMax.java
@@ -1,12 +1,15 @@
 package com.core.find;
+
 import java.lang.Number;
+
 import java.util.List;
+
+
 public class FindMax {
-	
-	
-	public static  Number findMax(List <? extends Number> myList) {
+
+	public static Number findMax(List <? extends Number> myList) {
 		for(Number n : myList) {
-			if(n.)
+			System.out.println(n);
 		}
 		return null;
 	}
--- java_mote/day13/src/tester/TestMax.java
+++ java_mote/day13/src/tester/TestMax.java
@@ -1,4 +1,5 @@
 package tester;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.lang.Number;
@@ -6,9 +7,16 @@ import com.core.find.*;
 public class TestMax {
 
 	public static void main(String[] args) {
-		List<Integer> newList = Arrays.asList(1,4,34,7,8,9);
+		List<Integer> newList = new ArrayList<>(Arrays.asList(1,4,34,7,8,9));
+		
+		List<Double> newList2 = new ArrayList<>(Arrays.asList(1.1,4.4,3.4,4.7,4.8,3.9));
+		
 		
 		FindMax.findMax(newList);
+		FindMax.findMax(newList2);
+		
+		
+		
 		for (Number n : newList) {
 			System.out.println(n.getClass());
 		}
