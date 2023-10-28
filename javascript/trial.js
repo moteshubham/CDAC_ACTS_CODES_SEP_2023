127d16737028a9f6c5acc443dc3d0676e9a56b57
Shubham Mote <moteshubham@gmail.com>
2023-10-28 16:19:07 +0530

    Update trial.js

--- javascript/trial.js
+++ javascript/trial.js
@@ -1,16 +1,3 @@
-// let quit = "qu";
-// let noquit = "qt";
-// let quRegex= /q(?!u)/;
-// let qRegex = /q(?=u)/;
-// console.log(quit.match(quRegex));
-// console.log(noquit.match(qRegex));
 
-// let str = "one two three";
-// let fixRegex = /(\w+)\s(\w+)\s(\w+)/; // Change this line
 
-// let replaceText = "$3 $2 $1"; // Change this line
-// let result = str.replace(fixRegex, replaceText);
-console.clear();
-let myArray = ["a", "b", "c", "d"][3];
-
-console.log(myArray);
+console.log("heasdllo");
