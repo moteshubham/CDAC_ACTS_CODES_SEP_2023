7e970d6b03fbd917249dcc1077f23da2315e4529
Shubham Mote <moteshubham@gmail.com>
2023-11-01 23:48:38 +0530

    Update trial.js

--- javascript/trial.js
+++ javascript/trial.js
@@ -1,14 +1,11 @@
 
+import axios from 'axios'
 
-function print(value) {
-  console.log(value);
-}
-
-function useCallback(callback) {
-  callback("Hello")
-}
-
-useCallback(print)
-
+const axios = require('axios');
+axios
+.get('https://www.boredapi.com/api/activity')
+.then(response =>{
+  console.log(`here : ${response.data.activity}`)
+})
 
 console.log("done");
