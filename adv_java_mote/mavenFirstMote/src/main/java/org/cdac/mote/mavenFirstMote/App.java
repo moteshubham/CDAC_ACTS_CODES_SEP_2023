1e4ccaa3691b4efdb1c4d2dc18a1d3f0a97ca6f1
Shubham Mote <moteshubham@gmail.com>
2023-11-23 21:35:17 +0530

    maven project 2

---
+++ adv_java_mote/mavenFirstMote/src/main/java/org/cdac/mote/mavenFirstMote/App.java
@@ -0,0 +1,13 @@
+package org.cdac.mote.mavenFirstMote;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
