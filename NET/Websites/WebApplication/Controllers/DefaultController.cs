dc2c3e3407c14e7265381a1486c6eb47b203c09d
Shubham Mote <moteshubham@gmail.com>
2023-12-15 14:37:59 +0530

    sdfsdf

---
+++ NET/Websites/WebApplication/Controllers/DefaultController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication.Controllers
+{
+    public class DefaultController : Controller
+    {
+        public IActionResult Index(int? id)
+        {
+           /* if (id == null)
+                return NotFound();
+            else*/
+                return View();
+        }
+
+        public IActionResult RenderDhanush(int? id)
+        {
+            return View();
+        }
+    }
+}
