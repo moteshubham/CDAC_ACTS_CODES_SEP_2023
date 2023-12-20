dd3c25f60dde1bd38ed4407876987801c500f484
Shubham Mote <moteshubham@gmail.com>
2023-12-20 08:11:15 +0530

    sd

---
+++ NET/Websites/EntityFrameworkApp/Models/Employee.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Build.Framework;
+using System;
+using System.ComponentModel.DataAnnotations;
+using RequiredAttribute = System.ComponentModel.DataAnnotations.RequiredAttribute;
+
+namespace EntityFrameworkApp.Models;
+
+public partial class Employee
+{
+    [Required(ErrorMessage = "EmpNo must be specified")]
+    public int EmpNo { get; set; }
+
+    [Required(ErrorMessage = "Name must be specified")]
+    public string Name { get; set; } = null!;
+
+    [Required(ErrorMessage = "Basic must be specified")]
+    [Range(100, 2000, ErrorMessage = "Basic must be between 100 and 2000")]
+    public decimal Basic { get; set; }
+
+    [Required(ErrorMessage = "Dept must be specified")]
+    public int DeptNo  { get; set; }
+
+    public virtual Department? DeptNoNavigation { get; set; } = null!;
+}
---
+++ NET/Websites/WebApp1/Models/Department.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebApp1.Models
+{
+    public class Department
+    {
+        [Column("DeptId", TypeName = "int")]
+        public int DeptId { get; set; }
+
+        [Column("DeptName", TypeName = "varchar(50)")]
+        public string DeptName { get; set; }
+    }
+}
