740a2547654ec64a66e6af093d55dd11c9277706
Shubham Mote <moteshubham@gmail.com>
2023-12-27 09:48:05 +0530

    sd

--- web_technologies/react/classfunctioncomponents/src/components/CounterClassComponent.js
+++ web_technologies/react/classfunctioncomponents/src/components/CounterClassComponent.js
@@ -1,26 +1,34 @@
-import React from "react";
+import React from "react"
 
 class CounterClassComponent extends React.Component {
   constructor(props) {
-    super(props);
+    super(props)
     this.state = {
       count: 0, //write any no of things whose value you want to retain in state
       name: "mote",
-    };
+    }
+    //this.increament=this.increament.bind(this)
   }
+  //---------------------------------------------------------------------------------------
   increment = () => {
     //this.setState(this.count++)   //you cant do this, cant directly modify this object
+    //use setstate function to change the state, it is asynchronous function, it will create new state object, it will not overrite the old state object
+    //to check the changed value of state use call back function
     this.setState({ ...this.state, count: this.state.count + 1 }, () => {
-      console.log(this.state.count); //here we get fresh output
-    });
+      console.log(this.state.count) //here we get fresh output
+    })
     //console.log(this.state.count); //if we dont write this line in clbk function it gives us the old value as setState is async
-  };
+  }
 
   decrement = () => {
     this.setState({ ...this.state, count: this.state.count <= 0 ? 0 : this.state.count - 1 }, () => {
-      console.log(this.state.count);
-    });
-  };
+      console.log(this.state.count)
+    })
+  }
+
+  resetcnt = () => {
+    this.setState({ ...this.state, count: 0 })
+  }
 
   render() {
     return (
@@ -32,12 +40,12 @@ class CounterClassComponent extends React.Component {
         <button type="button" className="btn btn-secondary" onClick={this.decrement} name="btn" id="dec">
           Decrement
         </button>
-        <button type="button" className="btn btn-danger" name="btn" id="res">
+        <button type="button" className="btn btn-danger" onClick={this.resetcnt} name="btn" id="res">
           Reset
         </button>
       </div>
-    );
+    )
   }
 }
 
-export default CounterClassComponent;
+export default CounterClassComponent
